fa06297457ef6d9825ee7b3d4bcb13824c01faf9
MuhammadIzzataArkananta <bankfree426@gmail.com>
2024-01-21 21:52:22 +0700

    variabel dan konstanta
    video #2

---
+++ js/variabel.js
@@ -0,0 +1,14 @@
+let angka = 10;
+let kata = "saya belajar js";
+
+const data = 100;
+
+angka = 5;
+
+console.log(data);
+
+console.log(angka);
+console.log(kata);
+
+let isi= 4;
+console.log(typeof isi);
